ad766d5cce1406b6a26e416b40f645388c2d9ab4
Elaine20.Chen <elaine20.chen@advantech.com.tw>
2025-04-10 13:40:56 +0800

    chore: update GitHub Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/demo-react/",
+  base: "/demo-react",
   plugins: [
     react(),
     createSvgIconsPlugin({
